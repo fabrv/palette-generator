406980bd03dc329e33aad85a6b503aa456acace6
Fabrizzio Rivera <frivera@nevicad.com>
2020-11-15 13:05:43 -0600

    compiled

--- dist/index.js
+++ dist/index.js
@@ -17,24 +17,87 @@ const template = `
   <meta name='viewport' content='width=device-width, initial-scale=1'>
   <style>
     html, body {
+      margin: 0;
+      padding: 0
       height: 100%;
       overflow: hidden;
+    }
+
+    td {
+      vertical-align:top;
+    }
+
+    td:hover span {
+      opacity: 1;
+    }
+
+    td span {
+      transition: all .3s;
+      opacity: 0;
+      padding: 10px; 
+      background-color: white; 
+      border-radius: 5px;
+      position: fixed;
+      left: 50%;
+      transform: translateX(-50%);
+      top: 25px;
+    }
+
+    #popup {
+      transition: all .3s;
+      opacity: 0;
+      padding: 10px; 
+      background-color: white; 
+      border-radius: 5px;
+      position: fixed;
+      left: 50%;
+      transform: translateX(-50%);
+      bottom: 25px;
+      z-index: 1000;
+    }
+    
+    .active {
+      opacity: 1 !important;
     }
   </style>
   {{{font}}}
 </head>
 <body>
-  <div style="position: fixed; width: 100%; top: 50%; transform: translateY(-50%); z-index: 1000; font-size: 10vw; text-align: center;">
-    <div style="color: white; opacity: 0.85">Lorem impsum.</div>
-    <div style="color: #222; opacity: 0.85">Sed euismod ex.</div>
+  <div id="popup">Copied to clipboard</div>
+  <input id="field" style="position: fixed; top: -100px">
+  <div style="position: fixed; width: 100%; top: 50%; transform: translateY(-50%); z-index: 1000; text-align: center;">
+    <div style="color: white; opacity: 0.85; font-size: 10vw;">Lorem impsum.</div>
+    <div style="color: #222; opacity: 0.85; font-size: 7vw;">Sed euismod ex.</div>
   </div>
   <table style="width: 100%; height: 100%; position: fixed; left: 0; right: 0;">
     <tr>
       {{#colors}}
-        <td style="background-color: {{.}};"></td>
+        <td onclick="clipboard('{{.}}')" style="background-color: {{.}};">
+        <span>{{.}}</span>
+        </td>
       {{/colors}}
     </tr>
   </table>
+
+  <script>
+    function clipboard(val) {
+      /* Get the text field */
+      var copyText = document.getElementById("field");
+      copyText.value = val
+
+      /* Select the text field */
+      copyText.select();
+      copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+      /* Copy the text inside the text field */
+      document.execCommand("copy");
+
+      document.getElementById('popup').classList.add('active')
+      setTimeout(() => {
+        document.getElementById('popup').classList.remove('active')
+      }, 3000);
+    }
+  </script>
 </body>
 </html>
 `;
@@ -54,9 +117,6 @@ const fonts = [
     `<link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
   <style>body{font-family: 'Anton';}</style>`,
-    `<link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Stalinist+One&display=swap" rel="stylesheet">
-  <style>body{font-family: 'Stalinist One';}</style>`,
     `<link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&display=swap" rel="stylesheet">
   <style>body{font-family: 'Dancing Script';}</style>`
